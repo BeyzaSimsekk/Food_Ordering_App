1cc83b299340495ecec9c4ae60ed052f0bd977c3
Beyza <u.beyza.simsek@gmail.com>
2025-08-16 21:23:11 +0300

    sentry kurulum

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -56,6 +56,7 @@ export default function Index() {
         <CartButton />
       </View>
       )}
+
       /> 
     </SafeAreaView>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,26 @@
+import * as Sentry from '@sentry/react-native';
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import "./global.css";
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://62038af8c999a07304db288447f9e2ca@o4509854979784704.ingest.us.sentry.io/4509855005343744',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const [fontsLoaded, error] = useFonts({
     "Quicksand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
     "Quicksand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
@@ -13,6 +30,8 @@ export default function RootLayout() {
   });
 
   const isAuthenticated = false; 
+
+  //ZUSTAND KURULUMUNDA KALDIM**********STORE*******2.07.39*****************
   
       
   useEffect(() => {
@@ -35,4 +54,4 @@ export default function RootLayout() {
       <Stack.Screen name="(tabs)" />
     </Stack>
   );
-}
+});

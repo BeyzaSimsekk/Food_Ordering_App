006a5b33f59bebe4f59521f6e17da8591d8cb8ba
Beyza <u.beyza.simsek@gmail.com>
2025-08-25 23:01:58 +0300

    useAppwrite hooku kullanılarak getMenu ve getCategories çağrıldı, tekrardan kaçınılmış oldu

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { getCategories, getMenu } from '@/lib/appwrite'
+import useAppwrite from '@/lib/useAppwrite'
+import { useLocalSearchParams } from 'expo-router'
+import React, { useEffect } from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-
 const Search = () => {
+
+  const {category, query} = useLocalSearchParams<{query: string, category: string }>()
+
+  const {data, refetch, loading} = useAppwrite({
+    fn: getMenu,
+    params:{category,query,limit: 6}
+  });
+
+  const { data: categories } = useAppwrite({
+    fn: getCategories
+  });
+
+  useEffect(()=>{
+    refetch({category, query, limit: 6});
+  },[category, query]) //parametreler değiştiğinde refetch yap
+
   return (
     <SafeAreaView>
       <Text>Search</Text>
